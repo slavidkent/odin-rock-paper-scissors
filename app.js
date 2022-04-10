cfc905ce9ea8bc7350263a7ed61885524c998d87
Kent <slavidkent@gmail.com>
2022-04-10 23:17:41 +0800

    Add javascript rock paper scissor game

--- app.js
+++ app.js
@@ -1,5 +1,47 @@
+let random;
 function computerPlay() {
-  const play = ["rock", "paper", "scissors"];
-  let random = Math.floor(Math.random()*3);
-  return play[random];
+    const play = ["rock", "paper", "scissors"];
+    random = Math.floor(Math.random() * 3);
+    return play[random];
 }
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    if (playerSelection == computerSelection) {
+        return `Draw! Both selected ${playerSelection} `;
+    } else if (computerSelection === "rock") {
+        if (playerSelection === "paper") {
+			winTotal++;
+            return "You Win! Paper beats Rock";
+        } else {
+            return "You Lose! Rock beats Scissor";
+        }
+    } else if (computerSelection === "paper") {
+        if (playerSelection === "scissors") {
+			winTotal++;
+            return "You Win! Scissors beats Paper";
+        } else {
+            return "You Lose! Paper beats Rock";
+        }
+    } else {
+        if (playerSelection === "rock") {
+			winTotal++;
+            return "You Win! Rock beat Scissor";
+        } else {
+            return "You Lose! Scissors beats Paper";
+        }
+    }
+}
+
+function game(){
+	for (let i=0;i<5;i++){
+		computerSelection = computerPlay();
+		console.log(playRound(playerSelection,computerSelection))
+	}
+	console.log("You win "+winTotal+" times")
+}
+
+const playerSelection = prompt("Rock, Paper, Scissors ?");
+let computerSelection;
+let winTotal = 0;
+game();
