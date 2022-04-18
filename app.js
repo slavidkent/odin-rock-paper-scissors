4dfc176586342d3077c38fc09cf4287aba28e4f0
Kent <slavidkent@gmail.com>
2022-04-18 21:06:47 +0800

    Add result screen and reset function

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ const playerScoresDisplay = document.querySelector('#player-score-number');
 const computerScoresDisplay = document.querySelector('#computer-score-number');
 const drawNumberDisplay = document.querySelector('#total-draw-number');
 const roundNumberDisplay = document.querySelector('#total-round-number');
+const gameEndSection = document.querySelector('.game-end');
+const displayWinLose = document.querySelector('#win-lose-game-end');
 
 // variable
 let roundNumber = 0;
@@ -20,7 +22,7 @@ let play = false;
 // event
 playButton.addEventListener('click', playClicked);
 buttons.forEach((button) => button.addEventListener('click', playRound));
-// reset.addEventListener('click', resetRound);
+reset.addEventListener('click', resetRound);
 
 // function
 // click play button
@@ -28,6 +30,7 @@ function playClicked() {
     play = true;
     grayOutContainer.forEach((element) => (element.style.display = 'none'));
     playButton.style.display = 'none';
+    display.textContent = 'Game Started...';
 }
 
 // random rock paper scissor generator
@@ -36,61 +39,63 @@ function computerPlay() {
     let random = Math.floor(Math.random() * 3);
     return play[random];
 }
+function capitalize(str) {
+    return str[0].toUpperCase() + str.slice(1);
+}
 
 // core game play mechanic
 function playRound(e) {
-    const playerSelection = e.currentTarget.value;
-    const computerSelection = computerPlay();
-    roundNumber += 1;
+    if (play) {
+        const playerSelection = e.currentTarget.value;
+        const computerSelection = computerPlay();
+        roundNumber += 1;
 
-    console.log('I play       : ' + e.currentTarget.value);
-    console.log('computer play: ' + computerSelection);
+        displaySelection(playerSelection, computerSelection);
 
-    displaySelection(playerSelection, computerSelection);
-
-    if (playerSelection === computerSelection) {
-        drawNumber += 1;
-        display.textContent = `Draw! Both selected ${playerSelection} `;
-    } else if (computerSelection === 'rock') {
-        if (playerSelection === 'paper') {
-            playerScore++;
-            display.textContent = 'You Win! Paper beats Rock';
+        if (playerSelection === computerSelection) {
+            drawNumber += 1;
+            display.textContent = `Draw! Both selected ${capitalize(playerSelection)}`;
+        } else if (computerSelection === 'rock') {
+            if (playerSelection === 'paper') {
+                playerScore++;
+                display.textContent = 'You Win! Paper beats Rock';
+            } else {
+                computerScore++;
+                display.textContent = 'You Lose! Rock beats Scissor';
+            }
+        } else if (computerSelection === 'paper') {
+            if (playerSelection === 'scissors') {
+                playerScore++;
+                display.textContent = 'You Win! Scissors beats Paper';
+            } else {
+                computerScore++;
+                display.textContent = 'You Lose! Paper beats Rock';
+            }
         } else {
-            computerScore++;
-            display.textContent = 'You Lose! Rock beats Scissor';
+            if (playerSelection === 'rock') {
+                playerScore++;
+                display.textContent = 'You Win! Rock beat Scissor';
+            } else {
+                computerScore++;
+                display.textContent = 'You Lose! Scissors beats Paper';
+            }
+            space - between;
         }
-    } else if (computerSelection === 'paper') {
-        if (playerSelection === 'scissors') {
-            playerScore++;
-            display.textContent = 'You Win! Scissors beats Paper';
-        } else {
-            computerScore++;
-            display.textContent = 'You Lose! Paper beats Rock';
+
+        roundNumberDisplay.textContent = roundNumber;
+        playerScoresDisplay.textContent = playerScore;
+        drawNumberDisplay.textContent = drawNumber;
+        computerScoresDisplay.textContent = computerScore;
+
+        if (playerScore >= 5) {
+            displayWinLose.textContent = 'Congratulation! You Win!!!';
+            stopGame();
+        } else if (computerScore >= 5) {
+            displayWinLose.textContent = 'Sorry! Computer Win, Git Good!';
+            stopGame();
         }
     } else {
-        if (playerSelection === 'rock') {
-            playerScore++;
-            display.textContent = 'You Win! Rock beat Scissor';
-        } else {
-            computerScore++;
-            display.textContent = 'You Lose! Scissors beats Paper';
-        }
-    }
-
-    console.log('Round Number:   ' + roundNumber);
-    console.log('player score:   ' + playerScore);
-    console.log('computer score: ' + computerScore);
-    console.log('Draw Amount:    ' + drawNumber);
-
-    roundNumberDisplay.textContent = roundNumber;
-    playerScoresDisplay.textContent = playerScore;
-    drawNumberDisplay.textContent = drawNumber;
-    computerScoresDisplay.textContent = computerScore;
-
-    if (playerScore >= 5) {
-        display.textContent = 'Congratulation! You Win 5 total scores.';
-    } else if (computerScore >= 5) {
-        display.textContent = 'Sorry! Computer Win 5 total scores, You lose Git Good';
+        alert('you should not be able to interact with the button, the game is not started, reload the page');
     }
 }
 
@@ -101,19 +106,30 @@ function displaySelection(p, c) {
         paper: '<i class="fa-solid fa-hand"></i>',
         scissors: '<i class="fa-solid fa-hand-scissors"></i>',
     };
-    choiceDisplay.forEach(element=>element.classList.remove('hide'))
-    console.log(choiceDisplay[1])
+    choiceDisplay.forEach((element) => element.classList.remove('hide'));
     choiceDisplay[1].innerHTML = displayObject[p];
     choiceDisplay[0].innerHTML = displayObject[c];
 }
 
 // stop game function
-function stopGame() {}
+function stopGame() {
+    gameEndSection.classList.remove('hide');
+    play = false;
+}
 
 // reset button behavior
 function resetRound() {
     playerScore = 0;
     computerScore = 0;
     roundNumber = 0;
+    drawNumber = 0;
     display.textContent = '';
+    play = true;
+    gameEndSection.classList.add('hide');
+    roundNumberDisplay.textContent = 0;
+    playerScoresDisplay.textContent = 0;
+    drawNumberDisplay.textContent = 0;
+    computerScoresDisplay.textContent = 0;
+    choiceDisplay.forEach((element) => element.classList.add('hide'));
+    playClicked();
 }
